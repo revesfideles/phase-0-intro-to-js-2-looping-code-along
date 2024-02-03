5d6be33b30994fb8c9a459150596798f99a28786
ashley <revesfideles@gmail.com>
2024-02-03 10:45:34 -0500

    Add myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    let thankYouMessages = [];
+
+    for (let i = 0; i < cards.length; i++) {
+        let message = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+        thankYouMessages.push(message);
+     debugger;
+    }
+
+    return thankYouMessages;
+}
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"]);
